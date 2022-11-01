afaea54d12e37edee56dddd67a05b6f0e2397ed2
sukyeongkang <sukyeong02@sookmyung.ac.kr>
2022-11-01 17:42:24 +0900

    실습 5

---
+++ main6.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define ROWS 3
+#define COLS 3
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+int main(int argc, char *argv[])
+ {
+	int A[ROWS][COLS] = {
+	{2,3,0},
+	{8,9,1},
+	{7,0,5}};
+	
+	int B[ROWS][COLS]={
+	{1,0,0},
+	{8,9,1},
+	{0,0,1}};
+	
+	int C[ROWS][COLS];
+	
+	addMatrix(A,B,C);
+	printMatrix(C);
+	
+	return 0;
+}
+
+void addMatrix(int A[][COLS], int B[][COLS], int C[][COLS], int size)
+{
+	int i, j;
+	
+	for (i=0; i<ROWS; i++)
+	{
+	
+	    for (j=0; j<COLS; j++)
+	      C[i][j]= A[i][j]+ B[i][j];
+	    printf("\n");
+    }
+}
+
+
+void printMatrix(int C[][COLS])
+{
+	int i, j;
+	
+	for(i=0; i<ROWS; i++)
+	{
+		for(j=0; j<COLS; j++)
+		printf("%3d", C[i][j]);
+		printf("\n");
+	}
+
+}
+
+
+
+
+
+
