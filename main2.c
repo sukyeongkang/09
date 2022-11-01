132aaa57c6a1f121f9281ae26e4c3eb613fa1b51
sukyeongkang <sukyeong02@sookmyung.ac.kr>
2022-11-01 16:30:03 +0900

    실습 2(1)

---
+++ main2.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define SIZE 5
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	
+	int i;
+	int grade[SIZE];
+	printf("5명의 점수를 입력하세요.\n");
+	
+	for(i=0; i<SIZE; i++)
+	  scanf("%d", &grade[i]);
+	
+	for(i=0; i<SIZE; i++) 
+	  printf("grade[%d] = %d\n", i, grade[i]);
+	  
+	
+	
+	return 0;
+}
